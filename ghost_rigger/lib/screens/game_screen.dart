f859b9db57cbf2a039239ea15bda9ae441b58a7e
Iain Smith <b099l3@gmail.com>
2020-06-27 15:38:41 +0100

    added some more stuff round the game

--- ghost_rigger/lib/screens/game_screen.dart
+++ ghost_rigger/lib/screens/game_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class GameScreen extends StatelessWidget {
   Widget build(context) {
     return Container(
-      margin: EdgeInsets.all(0.0),
       child: new Stack(
         children: <Widget>[
           Row(
@@ -29,9 +28,110 @@ class GameScreen extends StatelessWidget {
             ],
           ),
           new Center(
-            child: new Text(
-              "Hello",
-              style: TextStyle(color: Colors.white),
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 20.0),
+              margin: EdgeInsets.symmetric(vertical: 1.0, horizontal: 25.0),
+              child: AspectRatio(
+                aspectRatio: 1.75333,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        FlatButton(
+                          onPressed: () => {},
+                          padding: EdgeInsets.all(0.0),
+                          child: Image.asset(
+                            'assets/images/button_info.png',
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                        FlatButton(
+                          onPressed: () => {},
+                          padding: EdgeInsets.all(0.0),
+                          child: Image.asset('assets/images/button_info.png'),
+                        ),
+                        FlatButton(
+                          onPressed: () => {},
+                          padding: EdgeInsets.all(0.0),
+                          child: Image.asset('assets/images/button_info.png'),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            FlatButton(
+                              onPressed: () => {},
+                              padding: EdgeInsets.all(0.0),
+                              child:
+                                  Image.asset('assets/images/button_info.png'),
+                            ),
+                            FlatButton(
+                              onPressed: () => {},
+                              padding: EdgeInsets.all(0.0),
+                              child:
+                                  Image.asset('assets/images/button_info.png'),
+                            ),
+                            FlatButton(
+                              onPressed: () => {},
+                              padding: EdgeInsets.all(0.0),
+                              child:
+                                  Image.asset('assets/images/button_info.png'),
+                            ),
+                            FlatButton(
+                              onPressed: () => {},
+                              padding: EdgeInsets.all(0.0),
+                              child:
+                                  Image.asset('assets/images/button_info.png'),
+                            ),
+                            FlatButton(
+                              onPressed: () => {},
+                              padding: EdgeInsets.all(0.0),
+                              child:
+                                  Image.asset('assets/images/button_info.png'),
+                            ),
+                          ],
+                        ),
+                        Expanded(
+                          child: AspectRatio(
+                            aspectRatio: 1.5,
+                            child: Image.asset('assets/images/board.png'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        FlatButton(
+                          onPressed: () => {},
+                          padding: EdgeInsets.all(0.0),
+                          child: Image.asset('assets/images/button_info.png'),
+                        ),
+                        FlatButton(
+                          onPressed: () => {},
+                          padding: EdgeInsets.all(0.0),
+                          child: Image.asset('assets/images/button_info.png'),
+                        ),
+                        FlatButton(
+                          onPressed: () => {},
+                          padding: EdgeInsets.all(0.0),
+                          child: Image.asset('assets/images/button_info.png'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           )
         ],
