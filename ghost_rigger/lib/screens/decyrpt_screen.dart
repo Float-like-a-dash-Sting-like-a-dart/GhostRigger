084f4c6e0a7bd8c5d024470d425e30bc5ce38690
Iain Smith <b099l3@gmail.com>
2020-06-28 21:20:38 +0100

    decryptor

--- ghost_rigger/lib/screens/decyrpt_screen.dart
+++ ghost_rigger/lib/screens/decyrpt_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import '../animation/console_animation.dart';
 
 import '../main.dart';
 
-class DecryptScreen extends StatelessWidget {
+class DecryptScreen extends StatefulWidget {
+  @override
+  _DecryptScreenState createState() => _DecryptScreenState();
+}
+
+class _DecryptScreenState extends State<DecryptScreen> {
+  bool _showContinue = false;
   @override
   Widget build(BuildContext context) {
     final int nextLevel = ModalRoute.of(context).settings.arguments;
@@ -16,14 +23,80 @@ class DecryptScreen extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
-      child: OutlineButton(
-        padding: EdgeInsets.all(0.0),
-        color: Colors.transparent,
-        highlightedBorderColor: Colors.transparent,
-        onPressed: () =>
-            Navigator.pushReplacementNamed(context, '/level$nextLevel'),
-        child: null,
-      ),
+      child: Row(children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Flexible(
+                flex: 9,
+                fit: FlexFit.loose,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: getContent(),
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: _showContinue
+                    ? FlatButton(
+                        onPressed: () => setState(() {
+                          Navigator.pushReplacementNamed(
+                              context, '/level$nextLevel');
+                        }),
+                        child: Image.asset(
+                          'assets/images/intro_button_continue.png',
+                          height: 50,
+                          fit: BoxFit.contain,
+                        ),
+                      )
+                    : Container(),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
+  }
+
+  List<String> getText() {
+    switch (Main.currentLevel) {
+      case 2:
+        return [
+          "Great! You have now intercepted the messages sent by NDI.\n\n",
+          "But they seem to be encrypted, as expected.\n\n",
+          "Nothing you cannot deal with...",
+        ];
+      case 3:
+      default:
+        return [
+          "DECRYPTING MESSAGE...\n\n",
+          "The first prototypes for the new Super Pollutant Power Plant™ are ready to start the tests.\n\n",
+          "If they are successful, the plan to replace 5000 wind mills with these new oil-fired power plants will go ahead.\n\n",
+          "-- END OF MESSAGE --",
+        ];
+    }
+  }
+
+  ConsoleAnimatedTextKit getContent() {
+    var consoleAnimatedTextKit = ConsoleAnimatedTextKit(
+      displayFullTextOnTap: true,
+      text: getText(),
+      onFinished: () => setState(() {
+        _showContinue = true;
+      }),
+      textStyle: TextStyle(
+        fontSize: 20.0,
+        fontFamily: 'Rajdhani',
+        fontWeight: FontWeight.w500,
+      ),
+      textAlign: TextAlign.left,
+      alignment: Alignment.topCenter,
+    );
+
+    consoleAnimatedTextKit.createState();
+    return consoleAnimatedTextKit;
   }
 }
